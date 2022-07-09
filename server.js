3d10b9cefbf6de0ce04ebc06652bf11ae9f30673
Nirupama06 <87229371+Nirupama06@users.noreply.github.com>
2022-07-09 14:07:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ io.on('connection', socket =>{
     });
 });
 
-server.listen(3000,function(){
+server.listen(process.env.PORT||3000,function(){
     console.log('started server on port 3000')
 });
